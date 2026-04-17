df771e588ed1e34df57e18386e20cc43bec0282c
Elignobis <rwayisibongile@gmail.com>
2026-04-18 01:24:00 +0200

    fixing deployment of pages

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="hero">
-    <h1>Hi, I'm <span class="accent">Your Name</span></h1>
+    <h1>Hi, I'm <span class="accent">Sibongile</span></h1>
     <p>EdTech Developer & Learning Experience Designer</p>
     <a href="#projects" class="btn">View My Work</a>
   </section>
